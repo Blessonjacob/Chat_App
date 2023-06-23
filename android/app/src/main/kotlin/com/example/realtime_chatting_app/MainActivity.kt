f3f9207caa4e4b83a19b65786ca77205f68765d1
Blessonjacob <112881540+Blessonjacob@users.noreply.github.com>
2023-06-23 11:25:12 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/realtime_chatting_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.realtime_chatting_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+import'package:flutter/material.dart';
+class  Constants{
+  static  String  appId="1:34107101832:web:09e700ff4deb24c787053d";
+  static  String  apiKey="AIzaSyAs33JMcm-WCOHY7tZ7DzOkKprOIOwPob0";
+  static  String   messagingSenderId="34107101832";
+  static  String   projectId="chatappflutter-23fa1";
+  final primaryColor= const Color(0xFFee7b64);
+}
